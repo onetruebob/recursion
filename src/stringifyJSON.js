0848deb2020998dd42b19a67f3cb0329022cdce8
Bob Owen <bob@bobowen.org>
2014-05-04 16:32:19 -0400

    Created function for determining type of objects. Now handling all but objects.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,15 +4,30 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
 	// Let's determine the type of object we're dealing with so we know how to handle it.
-	var objType = typeof obj;
-	if (obj === null) { //special handling for null objects
+	var objType = getType(obj);
+	if (objType === "Null") { //special handling for null objects
 		return 'null';
-	} else if (objType === "undefined") { //special handling for undefined values
+	} else if (objType === "Undefined") { //special handling for undefined values
 		return;
-	} else if (objType === "number" || objType === "boolean") {
+	} else if (objType === "Number" || objType === "Boolean") {
 		return obj.toString();
-	} else if (objType === "string") { //strings must be quoted
+	} else if (objType === "String") { //strings must be quoted
 		return '"' + obj.toString() + '"';
+	} else if (objType === "Array") { //Step through array
+		var arrayJSON = "[";
+		obj.forEach(function (element) {
+			arrayJSON += stringifyJSON(element) + ",";
+		});
+		if (arrayJSON.charAt(arrayJSON.length - 1) === ",") { //trim trailing comma
+			arrayJSON = arrayJSON.slice(0, -1);
+		}
+		arrayJSON += "]";
+		return arrayJSON;
 	}
 
+};
+
+var getType = function(obj) {
+	var fullType = Object.prototype.toString.call(obj);
+	return fullType.split(" ")[1].slice(0, - 1);
 };
