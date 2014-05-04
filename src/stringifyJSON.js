ca77abe8548fae044732fbe2320810ad06ac842e
Bob Owen <bob@bobowen.org>
2014-05-04 16:11:03 -0400

    Starting down the path of determining and returning the terminating conditions of stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,16 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+	// Let's determine the type of object we're dealing with so we know how to handle it.
+	var objType = typeof obj;
+	if (obj === null) { //special handling for null objects
+		return 'null';
+	} else if (objType === "undefined") { //special handling for undefined values
+		return;
+	} else if (objType === "number" || objType === "boolean") {
+		return obj.toString();
+	} else if (objType === "string") { //strings must be quoted
+		return '"' + obj.toString() + '"';
+	}
+
 };
