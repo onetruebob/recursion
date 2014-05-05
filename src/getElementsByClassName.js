fb504e9ae65066310b2ed3bb01f2ad44fd19bf64
Bob Owen <bob@bobowen.org>
2014-05-04 21:24:23 -0400

    Created method of checking class name is in an element

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,4 +6,22 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
   // your code here
+  var returnElements = [];
+};
+
+var hasClassName = function (element, name) {
+	var classes = element.classList;
+
+	if (classes.length === 0) {
+		return false;
+	} else {
+		var nameMatch = false;
+
+		for(var i = 0; i < classes.length && !nameMatch; i++) {
+			if (classes[i] === name) {nameMatch = true};
+			console.dir(classes[i]);
+		}
+
+		return nameMatch;
+	}
 };
