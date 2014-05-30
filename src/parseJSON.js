b08f5406083fae42d3ef0a8bd772e3ca443db7dc
Bob Owen <bob@bobowen.org>
2014-05-29 19:28:38 -0700

    Created initial bones of JSONparse solution. Initial function looks at the first char to determine the kind of oject to parse and calls helper functions to complete the parsing.

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,37 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+	var returnObj, testChar, nextChar;
+
+	json = json.trim();
+	testChar = json.CharAt(0);
+	nextChar = json.CharAt(1);
+
+	if (testChar = '{') {
+		returnObj = parseObject(json);
+	} else if (testChar = '[') {
+		returnObj = parseArray(json);
+	} else if (testChar = '"' || testChar = "'") {
+		returnObj = parseString(json);
+	} else {
+		returnObj = parseNum(json);
+	}
+
+	return returnObj;
+};
+
+var parseObject = function(json) {
+
+};
+
+var parseArray = function(json) {
+
+};
+
+var parseString = function(json) {
+
+};
+
+var parseNum = function(json) {
+
 };
